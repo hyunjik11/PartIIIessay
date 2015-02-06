80d5a8391288c773269aa9825c1b52fe16a5eacb
hyunjik11 <hyunjik11@gmail.com>
2015-02-06 19:16:27 +0000

    change run script before running on screen

--- pmf.m
+++ pmf.m
@@ -1,16 +1,31 @@
-function [w1_P1,w1_M1,w1_P1_inc,w1_M1_inc]=pmf2(train_vec,probe_vec,epsilon,lambdau,lambdav,momentum, ...
+% Version 1.000
+%
+% Code provided by Ruslan Salakhutdinov
+%
+% Permission is granted for anyone to copy, use, modify, or distribute this
+% program and accompanying programs and documents for any purpose, provided
+% this copyright notice is retained and prominently displayed, along with
+% a note saying that the original programs are available from our
+% web page.
+% The programs and documents are distributed without any warranty, express or
+% implied.  As the programs were written for research purposes only, they have
+% not been tested to the degree that would be advisable in any important
+% application.  All use of these programs is entirely at the user's own risk.
+
+function [w1_P1,w1_M1,w1_P1_inc,w1_M1_inc]=pmf(train_vec,probe_vec,epsilon,lambdau,lambdav,momentum, ...
     maxepoch,numbatches,num_m,num_p,num_feat,w1_P1,w1_M1,w1_P1_inc,w1_M1_inc)
-%normalise ratings
-g=@(x) 1/(1+exp(-x));
-dg=@(x) exp(-x)/((1+exp(-x))^2);
+%this pmf is the most basic version, not applying g to (U^T)V and not
+%normalising the ratings
 
 rand('state',0); 
 randn('state',0); 
 
   %epsilon= Learning rate
-  % lambdau,lambdav=Regularization parameter
-  
+  % lambdau,lambdav=Regularization parameters
+
   epoch=1;
+
+  mean_rating = mean(train_vec(:,3)); 
  
   pairs_tr = length(train_vec); % number of training data 
   pairs_pr = length(probe_vec); % number of validation data 
@@ -40,23 +55,22 @@ for epoch = epoch:maxepoch
   clear rr 
 
   for batch = 1:numbatches
-    fprintf(1,'Epoch %d batch %d \r',epoch,batch);
+    fprintf(1,'epoch %d batch %d \r',epoch,batch);
     N=100000; % number training triplets per batch 
 
     aa_p   = double(train_vec((batch-1)*N+1:min(batch*N,pairs_tr),1)); %N rows for user col
     aa_m   = double(train_vec((batch-1)*N+1:min(batch*N,pairs_tr),2)); %N rows for movie col
     rating = double(train_vec((batch-1)*N+1:min(batch*N,pairs_tr),3)); %N rows of ratings
 
-    rating = (rating-1)/4; % rating scaled to between 0 and 1. 
+    rating = rating-mean_rating; % Default prediction is the mean rating. 
 
     %%%%%%%%%%%%%% Compute Predictions %%%%%%%%%%%%%%%%%
-    pred_out = arrayfun(g,sum(w1_M1(aa_m,:).*w1_P1(aa_p,:),2)); %g(U'V)
+    pred_out = sum(w1_M1(aa_m,:).*w1_P1(aa_p,:),2);
     %f = sum( (pred_out - rating).^2 + ...
-    %   lambda*( sum( (w1_M1(aa_m,:).^2 + w1_P1(aa_p,:).^2),2))); %value of minimising objective
+    %    lambda*( sum( (w1_M1(aa_m,:).^2 + w1_P1(aa_p,:).^2),2))); %value of minimising objective
 
     %%%%%%%%%%%%%% Compute Gradients %%%%%%%%%%%%%%%%%%%
-    grad_pred_out=arrayfun(dg,sum(w1_M1(aa_m,:).*w1_P1(aa_p,:),2)); %g'(U'V) : N by 1 col vector
-    IO = repmat(2*grad_pred_out.*(pred_out - rating),1,num_feat); % num_feat copies of 2g'(U'V)(U'V-R) concatenated
+    IO = repmat(2*(pred_out - rating),1,num_feat); % num_feat copies of 2*(pred_out - rating) concatenated
     %ie. is a N by num_feat matrix
     Ix_m=IO.*w1_P1(aa_p,:) + 2*lambdav*w1_M1(aa_m,:); %each row=gradient for Vj (j=1 to N)
     Ix_p=IO.*w1_M1(aa_m,:) + 2*lambdau*w1_P1(aa_p,:); %each row = gradient for Ui (i=1 to N)
@@ -76,7 +90,7 @@ for epoch = epoch:maxepoch
     end
     %%%% Update movie and user features %%%%%%%%%%%
 
-    w1_M1_inc = momentum*w1_M1_inc - epsilon*dw1_M1/N; 
+    w1_M1_inc = momentum*w1_M1_inc - epsilon*dw1_M1/N;
     w1_M1 =  w1_M1 + w1_M1_inc;
 
     w1_P1_inc = momentum*w1_P1_inc - epsilon*dw1_P1/N;
@@ -84,18 +98,17 @@ for epoch = epoch:maxepoch
   end 
 
   %%%%%%%%%%%%%% Compute Predictions after Parameter Updates %%%%%%%%%%%%%%%%%
-  pred_out = 1+4*arrayfun(g,sum(w1_M1(aa_m,:).*w1_P1(aa_p,:),2));
-  rating=1+4*rating;
+  pred_out = sum(w1_M1(aa_m,:).*w1_P1(aa_p,:),2);
   err_train(epoch) = sqrt(sum((pred_out- rating).^2)/pairs_tr);
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   %%% Compute predictions on the validation set %%%%%%%%%%%%%%%%%%%%%% 
-
+  
   aa_p = double(probe_vec(:,1));
   aa_m = double(probe_vec(:,2));
   rating = double(probe_vec(:,3));
 
-  pred_out = 1+4*arrayfun(g,sum(w1_M1(aa_m,:).*w1_P1(aa_p,:),2));
+  pred_out = sum(w1_M1(aa_m,:).*w1_P1(aa_p,:),2) + mean_rating;
   ff = find(pred_out>5); pred_out(ff)=5; % Clip predictions 
   ff = find(pred_out<1); pred_out(ff)=1;
 
@@ -105,8 +118,10 @@ for epoch = epoch:maxepoch
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
   if (rem(epoch,10))==0
-     save pmf_weights_and_errors2 w1_M1 w1_P1 w1_M1_inc w1_P1_inc err_valid
+     save pmf_weights_and_errors w1_M1 w1_P1 w1_M1_inc w1_P1_inc err_valid
   end
 
 end 
 end
+
+
